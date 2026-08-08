8805c1e9ada7a51fd33d7b9fee45281375a709b7
Sagittarius <your@email.com>
2026-08-09 00:48:12 +0530

    create dynamic cards inside board game

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const emoji = ["🤤","👅","🫦","😁","💀","😂","🥵","🤡"];
 
+const emojis = [...emoji,...emoji];
+
 const board = document.getElementById("board");
 
 
@@ -15,13 +17,28 @@ function addCard(card){
     board.appendChild(card);
 }
 
+function addFront(card){
+    const front = document.createElement("div");
+    card.appendChild(front);
+    front.classList.add("front");
+    return front;
+}
+
+function addBack(card){
+    const back = document.createElement("div");
+    card.appendChild(back)
+    back.classList.add("back");
+    return back;
+}
+
 function createDiv(){
 for(let  i = 0; i<16; i++){
     const card = document.createElement("div");
     classCard(card);
     addCard(card);
+    const front = addFront(card);
+    const back = addBack(card);
     }
 }
-
 
 createDiv();
